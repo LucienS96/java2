8fb3c6cf069593944ec686643d7fa2f05dcbf920
LucienS96 <tngrazer@web.de>
2017-02-10 18:44:41 +0100

    Update Materialartikel.java

--- Materialartikel.java
+++ Materialartikel.java
@@ -2,41 +2,41 @@ public class Materialartikel
 {	
 	public static void main(String args[])
 	{
-		Materialartikel = Materialartikel.new("Artname", "Artnr", "Einheit", "Packgr", "Mind", "Bestmenge", "Bestand");
+		materialartikel = Materialartikel.new("artname", "artnr", "einheit", "packgr", "mind", "bestmenge", "bestand");
 	}
 	
-	private String Artname;
-	private int Artnr;
-	private String Einheit;
-	private int Packgr;
-	private int Mind;
-	private int Bestmenge;
-	private int Bestand;
+	private String artname;
+	private int artnr;
+	private String einheit;
+	private int packgr;
+	private int mind;
+	private int bestmenge;
+	private int bestand;
 	
-	public Materialartikel(String Artname, int Artnr, String Einheit, int Packgr, int Mind, int Bestmenge, int Bestand)
+	public Materialartikel(String artname, int artnr, String einheit, int packgr, int mind, int bestmenge, int bestand)
 	{
-		this.Artname = Artname;
-		this.Artnr = Artnr;
-		this.Einheit = Einheit;
-		this.Packgr = Packgr;
-		this.Mind = Mind;
-		this.Bestmenge = Bestmenge;
-		this.Bestand = Bestand;
+		this.artname = artname;
+		this.artnr = artnr;
+		this.einheit = einheit;
+		this.packgr = packgr;
+		this.mind = mind;
+		this.bestmenge = bestmenge;
+		this.bestand = bestand;
 	}
 	
-	public void setArtname(String Artname){this.Artname = Artname;}
-	public void setArtnr(Integer Artnr){this.Artnr = Artnr;}
-	public void setEinheit(String Einheit){this.Einheit = Einheit;}
-	public void setPackgr(Integer Packgr){this.Packgr = Packgr;}
-	public void setMind(Integer Mind){this.Mind = Mind;}
-	public void setBestmenge(Integer Bestmenge){this.Bestmenge = Bestmenge;}
-	public void setBestand(Integer Bestand){this.Bestand = Bestand;}
+	public void setArtname(String artname){this.artname = artname;}
+	public void setArtnr(Integer artnr){this.artnr = artnr;}
+	public void setEinheit(String einheit){this.einheit = einheit;}
+	public void setPackgr(Integer packgr){this.Packgr = packgr;}
+	public void setMind(Integer mind){this.mind = mind;}
+	public void setBestmenge(Integer bestmenge){this.bestmenge = bestmenge;}
+	public void setBestand(Integer bestand){this.bestand = bestand;}
 	
-	public String getArtname(){return this.Artname;}
-	public Integer getArtnr() {return this.Artnr;}
-	public String getEinheit() {return this.Einheit;}
-	public Integer getPackgr()	{return this.Packgr;}
-	public Integer getMind() {return this.Mind;}
-	public Integer getBestmenge() {return this.Bestmenge;}
-	public Integer getBestand() {return this.Bestand;}
+	public String getArtname(){return this.artname;}
+	public Integer getArtnr() {return this.artnr;}
+	public String getEinheit() {return this.einheit;}
+	public Integer getPackgr() {return this.packgr;}
+	public Integer getMind() {return this.mind;}
+	public Integer getBestmenge() {return this.bestmenge;}
+	public Integer getBestand() {return this.bestand;}
 }
